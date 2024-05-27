2f8c099f4a941f9ec24d2a452f9807754ea3a99b
dz0nZ1 <dzonzi777@gmail.com>
2024-05-27 20:47:42 +0200

    Created serKeys

---
+++ social-media-app/data/swrKeys.ts
@@ -0,0 +1,34 @@
+export const SWR_KEYS = {
+
+    //AUTH KEYS
+
+    AUTH_BEGIN_LOGIN_KEY: "beginLogin",
+    AUTH_COMPLETE_LOGIN_WITHOUT_VALIDATION_TOKEN_KEY: "completeLogin",
+    AUTH_REGISTER_KEY: "register",
+    AUTH_REFRESH_TOKEN_KEY: "refresh",
+
+    //USER KEYS
+
+    USER_GET_KEY: "getUser",
+    USER_GET_ALL_KEY: 'getAllUsers',
+    USER_CREATE_KEY:  'createUser',
+    USER_UPDATE_KEY: "updateUser",
+    USER_DELETE_KEY: "deleteUser",
+ 
+    //POST KEYS
+
+    POST_GET_KEY: "getPost",
+    POST_GET_ALL_KEY: "getAllPosts",
+    POST_CREATE_KEY: "createPost",
+    POST_UPDATE_KEY: "updatePost",
+    POST_DELETE_KEY: "deletePost",
+
+    //COMMENT KEYS
+
+    COMMENT_GET_KEY: "getComment",
+    COMMENT_GET_ALL_KEY: "getAllComments",
+    COMMENT_CREATE_KEY: "createComment",
+    COMMENT_UPDATE_KEY: "updateComment",
+    COMMENT_DELETE_KEY: "deleteComment",
+
+ }
