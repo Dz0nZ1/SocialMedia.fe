85a6f67b854020ad878a4d585666e3e10e88b06a
dz0nZ1 <dzonzi777@gmail.com>
2024-05-27 20:47:01 +0200

    Created httpClient

---
+++ social-media-app/data/httpClient.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const Axios = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_REST_API_ENDPOINT,
+    timeout: 150000000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const getDefault = async <T>(url: string, params?: any, config?: any) => {
+    const response = await Axios.get<T>(url, {params, headers: config?.headers});
+    return response.data;
+}
+
+export const postDefault = async <T>(url: string, params?: any, config?: any) => {
+    const response = await Axios.post<T>(url, params, config);
+    return {data: response.data, status: response.status}
+}
